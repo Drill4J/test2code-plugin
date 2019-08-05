Drill4J/drill4j

EPMDJ-779. Implemented scope name validation while renaming/switching a scope

plugins/drill-coverage-plugin/src/commonMain/kotlin/Actions.kt
@@ -6,6 +6,10 @@ import kotlinx.serialization.*
 @Serializable
 abstract class Action
 
+@SerialName("VALIDATION")
+@Serializable
+data class Validate(val payload: String) : Action()
+
 @SerialName("START")
 @Serializable
 data class StartNewSession(val payload: StartPayload) : Action()

plugins/drill-coverage-plugin/src/commonMain/kotlin/SerDe.kt
@@ -12,6 +12,7 @@ private val serialModule = SerializersModule {
         addSubclass(DropScope.serializer())
 
         addSubclass(StartNewSession.serializer())
+        addSubclass(Validate.serializer())
         addSubclass(StartSession.serializer())
         addSubclass(StopSession.serializer())
         addSubclass(CancelSession.serializer())

Drill4J/test2code-plugin

src/commonMain/kotlin/Actions.kt
@@ -6,6 +6,10 @@ import kotlinx.serialization.*
 @Serializable
 abstract class Action
 
+@SerialName("VALIDATION_RESULT")
+@Serializable
+data class ValidationResult(val payload: String) : Action()
+
 @SerialName("START")
 @Serializable
 data class StartNewSession(val payload: StartPayload) : Action()

src/commonMain/kotlin/SerDe.kt
@@ -12,6 +12,7 @@ private val serialModule = SerializersModule {
         addSubclass(DropScope.serializer())
 
         addSubclass(StartNewSession.serializer())
+        addSubclass(ValidationResult.serializer())
         addSubclass(StartSession.serializer())
         addSubclass(StopSession.serializer())
         addSubclass(CancelSession.serializer())