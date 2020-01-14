44f37f95b485584a86c5926c166cd41dbe3658a8
Viktor Bellavin <viktor_bellavin@epam.com>
2020-01-14 15:39:00 +0300

    EPMDJ-2013 Service Group Summary by Plugin - null summary for unregistered agents.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ val serializationRuntimeVersion = "0.14.0"
 
 val atomicFuVersion = "0.14.1"
 
-val drillAdminVersion = "0.5.0-23"
+val drillAdminVersion = "0.5.0-25"
 
 val drillCommonVersion = "0.4.63"
 
