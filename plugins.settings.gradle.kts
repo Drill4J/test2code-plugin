5e146b20658b9690d778e5cad848ef5c17f4afae
Viktor Bellavin <viktor_bellavin@epam.com>
2020-03-18 15:18:13 +0300

    Moved plugin config into a separate gradle settings script.

---
+++ plugins.settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val atomicFuVersion: String by settings
+    val drillGradlePluginVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        id("kotlinx-atomicfu") version atomicFuVersion
+        id("com.epam.drill.version.plugin") version drillGradlePluginVersion
+        id("com.github.johnrengelman.shadow") version "5.2.0"
+    }
+
+    repositories {
+        gradlePluginPortal()
+        maven(url = "https://oss.jfrog.org/artifactory/list/oss-release-local")
+        maven(url = "https://dl.bintray.com/kotlin/kotlinx/")
+    }
+}
+
+//TODO remove after plugin marker is added to the atomicfu artifacts
+mapOf(
+    "kotlinx-atomicfu" to "org.jetbrains.kotlinx:atomicfu-gradle-plugin"
+).let { substitutions ->
+    pluginManagement.resolutionStrategy.eachPlugin {
+        substitutions["${requested.id}"]?.let { useModule("$it:${target.version}") }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,6 @@
 rootProject.name = "test2code-plugin"
 
-pluginManagement {
-    val kotlinVersion: String by settings
-    val atomicFuVersion: String by settings
-    val drillGradlePluginVersion: String by settings
-    plugins {
-        kotlin("jvm") version kotlinVersion
-        kotlin("plugin.serialization") version kotlinVersion
-        id("kotlinx-atomicfu") version atomicFuVersion
-        id("com.epam.drill.version.plugin") version drillGradlePluginVersion
-        id("com.github.johnrengelman.shadow") version "5.2.0"
-    }
-    repositories {
-        gradlePluginPortal()
-        maven(url = "https://oss.jfrog.org/artifactory/list/oss-release-local")
-        maven(url = "https://dl.bintray.com/kotlin/kotlinx/")
-    }
-    val substitutions = mapOf("kotlinx-atomicfu" to "org.jetbrains.kotlinx:atomicfu-gradle-plugin")
-    resolutionStrategy.eachPlugin {
-        substitutions["${requested.id}"]?.let { useModule("$it:${target.version}") }
-    }
-}
+apply(from = "plugins.settings.gradle.kts")
 
 include(":admin-part")
 include(":agent-part")
