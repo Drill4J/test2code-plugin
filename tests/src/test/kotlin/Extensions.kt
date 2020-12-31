8efb7d26549259733cb188e779fb29fdc506c3a9
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2020-12-31 10:00:52 +0300

    EPMDJ-5821 Updated drill-api to v0.7.0-8

--- tests/src/test/kotlin/Extensions.kt
+++ tests/src/test/kotlin/Extensions.kt
@@ -1,7 +1,7 @@
 package com.epam.drill.plugins.test2code
 
 import com.epam.drill.admin.common.*
-import com.epam.drill.common.*
+import com.epam.drill.admin.common.serialization.*
 import com.epam.drill.plugins.test2code.api.*
 import com.epam.drill.plugins.test2code.common.api.*
 import io.ktor.application.*
