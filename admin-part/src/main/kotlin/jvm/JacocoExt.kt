70fc807d2244b738c083b17e4c401195446fae23
Roman <roman_ivanov3@epam.com>
2021-06-29 01:33:22 +0300

    Fix parallel bundle analysis

--- admin-part/src/main/kotlin/jvm/JacocoExt.kt
+++ admin-part/src/main/kotlin/jvm/JacocoExt.kt
@@ -23,6 +23,8 @@ import com.epam.drill.plugins.test2code.util.*
 import com.epam.kodux.util.*
 import org.jacoco.core.analysis.*
 import org.jacoco.core.data.*
+import org.jacoco.core.internal.analysis.*
+import java.util.concurrent.*
 
 private val logger = logger {}
 
@@ -37,7 +39,7 @@ internal fun Sequence<ExecClassData>.bundle(
             runCatching {
                 analyzer.analyzeClass(classesBytes, execData.name)
             }.onFailure {
-                logger.error { "Error while analyzing ${execData.name}." }
+                logger.error(it) { "Error while analyzing ${execData.name}." }
             }
         } ?: println("WARN No class data for ${execData.name}, id=${execData.id}")
     }
@@ -53,9 +55,9 @@ internal fun Iterable<String>.bundle(
 private fun Sequence<ExecClassData>.bundle(
     probeIds: Map<String, Long>,
     analyze: ExecutionDataStore.(Analyzer) -> Unit,
-): IBundleCoverage = CoverageBuilder().also { coverageBuilder ->
+): IBundleCoverage = CustomCoverageBuilder().also { coverageBuilder ->
     val dataStore = execDataStore(probeIds)
-    val analyzer = Analyzer(dataStore, coverageBuilder)
+    val analyzer = Analyzer(dataStore, coverageBuilder).fixConcurrencyIssues()
     dataStore.analyze(analyzer)
 }.getBundle("")
 
@@ -181,4 +183,19 @@ fun parseDescType(char: Char, charIterator: CharIterator): String = when (char) 
 
 private fun ExecClassData.toExecutionData(probeIds: Map<String, Long>): ExecutionData? = probeIds[className]?.let {
     ExecutionData(it, className, probes.toBooleanArray())
+}
+
+private fun Analyzer.fixConcurrencyIssues() = also { analyzer ->
+    val newPool = StringPool()
+    StringPool::class.java.getDeclaredField("pool").apply {
+        isAccessible = true
+        set(newPool, ConcurrentHashMap<String, String>())
+        isAccessible = false
+    }
+    Analyzer::class.java.getDeclaredField("stringPool").apply {
+        isAccessible = true
+        set(analyzer, newPool)
+        isAccessible = false
+    }
+
 }
