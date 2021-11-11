f9197e97d50ac017ae05c297b909c2d41f73123b
LikhanovMaxim <LikhanovMaxim@yandex.ru>
2021-11-11 18:23:37 +0300

    EPMDJ-8997 Integrate postgres DSM refactoring

--- admin-part/src/main/kotlin/Plugin.kt
+++ admin-part/src/main/kotlin/Plugin.kt
@@ -26,6 +26,7 @@ import com.epam.drill.plugins.test2code.coverage.*
 import com.epam.drill.plugins.test2code.group.*
 import com.epam.drill.plugins.test2code.storage.*
 import com.epam.drill.plugins.test2code.util.*
+import com.epam.dsm.*
 import com.epam.kodux.util.*
 import com.github.luben.zstd.*
 import kotlinx.atomicfu.*
@@ -35,7 +36,6 @@ import kotlinx.serialization.protobuf.*
 import java.io.*
 import java.util.*
 import java.util.concurrent.*
-import com.epam.dsm.*
 
 internal object AsyncJobDispatcher : CoroutineScope {
     override val coroutineContext =
