0fdabcff7df3c1ec34a9c95ef1039e9be3fce2bb
ViktoriiaLebedeva <Viktoriia_Lebedeva@epam.com>
2022-04-04 15:04:10 +0300

    EPMDJ-10318 Added ability to store field in separate Column

--- admin-part/src/test/kotlin/perf/PerformanceTest.kt
+++ admin-part/src/test/kotlin/perf/PerformanceTest.kt
@@ -50,7 +50,7 @@ class PerformanceTest : PluginTest() {
         switchScopeWithProbes()
     }
 
-    @Ignore(value = "it takes a lot of time, remove it after EPMDJ-10318")
+    @Ignore(value = "Failed with OOM")
     @Test
     fun `perf check! should finish scope with 2 session and takes probes`() = runBlocking {
         switchScopeWithProbes(800)
--- agent-api/build.gradle.kts
+++ agent-api/build.gradle.kts
@@ -4,10 +4,13 @@ plugins {
     `maven-publish`
 }
 
+val drillDsmVersion: String by extra
+
 kotlin {
     sourceSets.commonMain {
         dependencies {
             compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core")
+            implementation("com.epam.drill.dsm:annotations:$drillDsmVersion")
         }
     }
 
