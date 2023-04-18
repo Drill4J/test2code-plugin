b307e7be933fbfa6cb5d8d14fdfafb6eca0567d8
RomanDavlyatshin <davlyatshinra@gmail.com>
2023-04-18 19:25:37 +0300

    Feature/cleanup test2code tables on build deletion (#386)
    * dsm-debug
    
    * dsm-debug
    
    * feat!: add agentKey to Binarya table
    
    N.B: decode fails for BynariaData.
    
    * feat: add method to clean up Binarya table
    
    * feat: EPMDJ-10551 clean up tables
    - add methods to clean up string_to_bundlecounter,  testkey_to_bundlecounter, testkey_to_testoverview, string_to_list table, exec_class_data tables
    
    * fix: change version related to pushed tags
    
    * fix: change method to remove tables
    
    * fix: change version for EPMDJ-10551
    
    * fix: code convention & clean up code
    
    * build: set dsm version to support build data deletion
    
    ---------
    
    Co-authored-by: Bohdan <bohdan_asieiev@epam.com>

--- admin-part/src/main/kotlin/storage/Build.kt
+++ admin-part/src/main/kotlin/storage/Build.kt
@@ -16,10 +16,13 @@
 package com.epam.drill.plugins.test2code.storage
 
 import com.epam.drill.plugins.test2code.*
+import com.epam.drill.plugins.test2code.api.*
+import com.epam.drill.plugins.test2code.common.api.*
 import com.epam.drill.plugins.test2code.coverage.*
 import com.epam.drill.plugins.test2code.util.*
 import com.epam.dsm.*
 import com.epam.dsm.find.*
+import com.epam.dsm.serializer.*
 import kotlinx.serialization.*
 
 @Serializable
@@ -85,6 +88,33 @@ internal suspend fun CachedBuild.store(storage: StoreClient) {
 internal suspend fun StoreClient.removeBuild(
     agentKey: AgentKey,
 ) = executeInAsyncTransaction {
+    deleteById<ActiveScopeInfo>(agentKey)
+    deleteById<TestsToRunSummary>(agentKey)
+
+    val agentKeyHashCode = "${agentKey.hashCode()}"
+    deleteBy<ClassCounter> { containsParentId(listOf(agentKeyHashCode)) }
+    deleteBy<JavaClassCoverage> { containsParentId(listOf(agentKeyHashCode)) }
+    deleteBy<MethodCounter> { containsParentId(listOf(agentKeyHashCode)) }
+    deleteBy<JavaMethodCoverage> { containsParentId(listOf(agentKeyHashCode)) }
+
+    sessionIds(agentKey).forEach {
+        deleteBy<StoredSession> { FieldPath(StoredSession::agentKey, AgentKey::agentId) eq agentKey.agentId }
+        deleteBy<ExecClassData> { containsParentId(listOf(it)) }
+        deleteBy<Label> { containsParentId(listOf(it)) }
+        deleteBy<TestOverview> { containsParentId(listOf(it)) }
+    }
+
+    cleanUpBinaryTable("agent::${agentKey.agentId}:${agentKey.buildVersion}:")
+
+    //clean up string_to_bundlecounter
+    deleteMapByParentId(parentId = agentKey, entryClass = EntryClass(String::class, BundleCounter::class))
+    //clean up testkey_to_bundlecounter
+    deleteMapByParentId(parentId = agentKey, entryClass = EntryClass(TestKey::class, BundleCounter::class))
+    //clean up testkey_to_testoverview
+    deleteMapByParentId(parentId = agentKey, entryClass = EntryClass(TestKey::class, TestOverview::class))
+    //clean up string_to_list
+    deleteMapByParentId(parentId = agentKey, entryClass = EntryClass(String::class, List::class))
+
     deleteById<BuildStats>(agentKey)
     deleteById<StoredBundles>(agentKey)
     deleteById<StoredBuildTests>(agentKey)
