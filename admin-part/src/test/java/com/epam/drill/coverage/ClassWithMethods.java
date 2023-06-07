7ef9566ca34f5e2028274aaf328bc796f0a38cfe
iryabov <to.iryabov@gmail.com>
2023-06-07 12:53:55 +0200

    docs: add java docs form Model classes EPMDJ-10590

---
+++ admin-part/src/test/java/com/epam/drill/coverage/ClassWithMethods.java
@@ -0,0 +1,19 @@
+package com.epam.drill.coverage;
+
+public class ClassWithMethods {
+    public ClassWithMethods() {
+        super();
+        System.out.println("0,1");
+    }
+    public void method1() {
+        System.out.println("2");
+        System.out.println("3");
+    }
+    public void method2() {
+        System.out.println("4");
+    }
+    public void method3() {
+        System.out.println("5");
+        System.out.println("6");
+    }
+}
