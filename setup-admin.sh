cfe66cd5232e2ee9db5aaf07a83843091b91fdf8
epamrsa <epamrsa@gmail.com>
2023-07-17 19:42:05 +0300

    feat: Implement import of admin git-repo instead of mavent artifacts for test2core-plugin tests
    Implement gradle scripts and git workflows
    
    Refs: EPMDJ-10619

--- setup-admin.sh
+++ setup-admin.sh
@@ -5,5 +5,5 @@ export `grep adminRef gradle.properties | tr -d [:space:]`
 echo 'Removing admin directory'
 rm -rf admin
 
-echo 'Cloning https://github.com/Drill4J/admin repository with branch' $admin
-git clone https://github.com/Drill4J/admin admin --branch $admin
+echo 'Cloning https://github.com/Drill4J/admin repository with branch' $adminRef
+git clone https://github.com/Drill4J/admin admin --branch $adminRef
